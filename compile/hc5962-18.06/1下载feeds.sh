413cdf81141346378e88c180fe0329d84188421a
kjxhb <119322320+kjxhb@users.noreply.github.com>
2026-04-05 12:09:59 +0800

    更改仓库

--- compile/hc5962-18.06/1下载feeds.sh
+++ compile/hc5962-18.06/1下载feeds.sh
@@ -1,4 +1,4 @@
-git clone --depth 1 https://github.com/coolsnowwolf/lede openwrt
+git clone --depth 1 https://github.com/kjxhb/lede openwrt
 cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
